9d25afc4a5d1e028c7f9f69b454b1077fd28ab17
Joakim224 <joakimbusiness224@gmail.com>
2023-09-25 15:46:09 +0200

    tilføjet user interface

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    Scanner scanner = new Scanner(System.in);
+
+    public void startProgram(){
+        /*adventure = new Adventure();*/
+        welcome();
+    }
+    public void welcome() {
+        System.out.println("WELCOME TO THE CAVE EXPLORER: 1.0" + "\n ");
+        System.out.println("Menu: " +
+                "\nType GO + the direction you want to move towards (North,South,East,West). " +
+                "\nType HELP to display instructions and information about possible commands." +
+                "\nType LOOK to see current room " +
+                "\nType EXIT to exit the game.");
+        direction();
+    }
+
+    public void direction() {
+        String input = "";
+        input = scanner.next();
+        while (!input.equals("exit")) {
+            switch (input) {
+                case "go south":
+                    System.out.println("Going South");
+                    break;
+                case "go north":
+                    System.out.println("Going North");
+                    break;
+
+                case "go west":
+                    System.out.println("Going West");
+                    break;
+
+                case "go east":
+                    System.out.println("Going East");
+                    break;
+
+                case "exit":
+                    endProgram();
+                    break;
+
+                case "look":
+                    System.out.println("currentroom");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice, try again");
+                    break;
+
+            }
+        }
+    }
+
+    public void endProgram(){
+        System.out.println("Game ended.");
+    }
+
+}
