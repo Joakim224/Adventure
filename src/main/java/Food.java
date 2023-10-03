a5d627b5eeff4205cfd19cd76a46946653cf1af6
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2023-10-03 13:11:16 +0200

    tilføjet konstruktør til Food

--- src/main/java/Food.java
+++ src/main/java/Food.java
@@ -1,6 +1,15 @@
-public class Food {
+public class Food extends Item {
     private int healpoints;
+
+
+   public Food(String itemName, String itemDescription, int healpoints){
+       super(itemName, itemDescription);
+       this.healpoints = healpoints;
+
+   }
     public int getHealpoints(){
         return healpoints;
+
+
     }
 }
