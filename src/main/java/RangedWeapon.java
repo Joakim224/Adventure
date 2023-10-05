a061bd30cd8fd1f9411f76a49f11b36ecfe25a5d
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2023-10-05 08:50:31 +0200

    tilføjet RangedWeapon

---
+++ src/main/java/RangedWeapon.java
@@ -0,0 +1,11 @@
+public class RangedWeapon extends Weapon{
+    private int ammo;
+
+    public RangedWeapon(String itemName, String itemDescription, int damage,  int ammo){
+        super(itemName, itemDescription, damage);
+        this.ammo = ammo;
+    }
+    public int getAmmo(){
+        return ammo;
+    }
+}
