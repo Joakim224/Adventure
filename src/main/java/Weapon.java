f78074d5ff1912b648efefdccb3e3fef59dba6e9
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2023-10-05 08:44:52 +0200

    tilføjet Weapon class

---
+++ src/main/java/Weapon.java
@@ -0,0 +1,8 @@
+public class Weapon extends Item {
+    private int damage;
+
+    public Weapon(String itemName, String itemDescription, int damage){
+        super(itemName, itemDescription);
+        this.damage = damage;
+    }
+}
